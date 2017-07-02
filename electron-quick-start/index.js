85f7a73b2e818214f3f26e849a325ef9fd717ba7
Landon <landon@angrydrop.net>
2017-07-02 03:50:36 -0400

    Added robotjs to send Keystrokes, added functionality for Auto paste on Selection

--- electron-quick-start/index.js
+++ electron-quick-start/index.js
@@ -5,9 +5,14 @@ const currentWindow = electron.remote.getCurrentWindow();
 const settings = currentWindow.settings;
 
 $('#setting-open-at-cursor').prop('checked', settings.openAtCursorPosition);
+$('#setting-auto-paste-selection').prop('checked', settings.autoPasteOnSelection);
 
 $('#setting-open-at-cursor').change(function cc() {
   ipc.send('setting-change-openAtCursor', this.checked);
+});
+
+$('#setting-auto-paste-selection').change(function cc() {
+  ipc.send('setting-change-autoPasteOnSelection', this.checked);
 });
 
 $('#hide-to-tray-btn').click(() => {
